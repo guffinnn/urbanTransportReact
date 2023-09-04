747fa3965d73e84be56bf1e1b86bc4e341fa4fbf
Danik <dashkevich_daniil2006@mail.ru>
2023-09-04 11:14:58 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
 let addEventsForInputs = input => {
     input.addEventListener('click', () => {
-        if(input === signUpButton) {
-            window.location.href = './pages/sign_up.html';
-        }
-        else if(input === logInButton) {
-            window.location.href = './pages/log_in.html';
-        }
-        else if(input === mapButton) {
-            window.location.href = './pages/map.html';
+        switch(input) {
+            case signUpButton:
+                window.location.href = './pages/sign_up.html'
+                break
+            case logInButton:
+                window.location.href = './pages/log_in.html'
+                break
+            case mapButton:
+                window.location.href = './pages/map.html';
+                break
+            case searchButton:
+                window.location.href = './pages/search.html';
+                break
+            default:
+                return 0
         }
     });
 }
 
 const signUpButton = document.querySelector('#sign__up__default'),
       logInButton = document.querySelector('#log__in__default'),
-      mapButton = document.querySelector('#map');
+      mapButton = document.querySelector('#map'),
+      searchButton = document.querySelector('#catalog');
 
 addEventsForInputs(signUpButton);
 addEventsForInputs(logInButton);
 addEventsForInputs(mapButton);
+addEventsForInputs(searchButton);
