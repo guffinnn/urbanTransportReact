f4436c8a7895b0a9b92e872fb731e5a32ad32f93
Danik <dashkevich_daniil2006@mail.ru>
2023-09-05 10:24:04 +0300

    add a styles for add-to-cart_box, dinamic add-to-cart??

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
-export default function App() {
-  return <h2>Hello world!</h2>;
+// import RouteCard from './components/ExpenseItem';
+
+// Это JSX-code (JavaScript and XML)
+function App() {
+  return (
+      // Начало XML кода
+      <div>
+        <h1>Let's get started!</h1>
+      </div>
+  );
 }
+
+export default App;
